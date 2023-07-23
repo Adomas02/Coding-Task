dfa888cd222e4f8c4719cec6d25a0e421d034e63
Adomas02 <kazen.adomas@gmail.com>
2023-07-23 18:22:42 +0300

    updated test

--- src/test/java/lt/adomas/codingtask/CodingTaskServiceTest.java
+++ src/test/java/lt/adomas/codingtask/CodingTaskServiceTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class CodingTaskServiceTest {
