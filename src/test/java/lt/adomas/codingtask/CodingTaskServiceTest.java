9f8cef50b9d44ee79a5e0cb8683c86a6d0f47bcc
Adomas02 <kazen.adomas@gmail.com>
2023-07-23 18:22:01 +0300

    updated test

--- src/test/java/lt/adomas/codingtask/CodingTaskServiceTest.java
+++ src/test/java/lt/adomas/codingtask/CodingTaskServiceTest.java
@@ -22,16 +22,21 @@ class CodingTaskServiceTest {
     @Test
     void shouldGroupByFile() {
 
-        var testInputFileContent = "bb AA Cc aa tEst-word:d ((house))";
+        var testInputFileContent = "bb AA Cc aa tEst-word:d ((house))    ---great-Work@---";
 
         var actualResult = codingTaskService.groupingByFiles(testInputFileContent);
 
         assertEquals("AA", actualResult.get("Result_A-G.txt").get(0).getWord());
         assertEquals(2, actualResult.get("Result_A-G.txt").get(0).getFrequency());
+
         assertEquals("TEST-WORD", actualResult.get("Result_O-U.txt").get(0).getWord());
         assertEquals(1, actualResult.get("Result_O-U.txt").get(0).getFrequency());
+
         assertEquals("HOUSE", actualResult.get("Result_H-N.txt").get(0).getWord());
         assertEquals(1, actualResult.get("Result_H-N.txt").get(0).getFrequency());
+
+        assertEquals("GREAT-WORK", actualResult.get("Result_A-G.txt").get(4).getWord());
+        assertEquals(1, actualResult.get("Result_A-G.txt").get(4).getFrequency());
 
     }
 
